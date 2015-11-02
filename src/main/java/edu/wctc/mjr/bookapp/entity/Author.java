148bccdc503a606fce9c01c0698bc9d86a805495
Matthew <Matthew@192.168.1.107>
2015-11-02 16:55:31 -0600

    Fixed URI resolution problem.

--- src/main/java/edu/wctc/mjr/bookapp/entity/Author.java
+++ src/main/java/edu/wctc/mjr/bookapp/entity/Author.java
@@ -9,8 +9,10 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -49,7 +51,7 @@ public class Author implements Serializable {
     @Column(name = "date_created")
     @Temporal(TemporalType.DATE)
     private Date dateCreated;
-    @OneToMany(mappedBy = "authorId")
+    @OneToMany(mappedBy = "authorId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Set<Book> bookSet;
 
     public Author() {
@@ -116,5 +118,5 @@ public class Author implements Serializable {
     public String toString() {
         return "edu.wctc.mjr.bookapp.entity.Author[ authorId=" + authorId + " ]";
     }
-    
+   
 }
--- src/main/java/edu/wctc/mjr/bookapp/repository/AuthorRepository.java
+++ src/main/java/edu/wctc/mjr/bookapp/repository/AuthorRepository.java
@@ -7,12 +7,17 @@ package edu.wctc.mjr.bookapp.repository;
 
 import edu.wctc.mjr.bookapp.entity.Author;
 import java.io.Serializable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author Matthew
  */
 public interface AuthorRepository extends JpaRepository<Author, Integer>, Serializable{
+    
+    @Query("SELECT a FROM Author a JOIN FETCH a.bookSet WHERE a.authorId = (:id)")
+    public Author findByIdAndFetchBooksEagerly(@Param("id") Integer id);
     
 }
